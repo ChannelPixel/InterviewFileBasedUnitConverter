74a270896d179082bd3b58bead270de224050e2b
ChannelPixel <ChannelPixel@users.noreply.github.com>
2019-12-01 16:28:47 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AlcolizerFileBasedUnitConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_InputPath = new System.Windows.Forms.TextBox();
+            this.Txt_OutputPath = new System.Windows.Forms.TextBox();
+            this.Txt_OutputFileName = new System.Windows.Forms.TextBox();
+            this.Btn_BrowseOutput = new System.Windows.Forms.Button();
+            this.Btn_BrowseInput = new System.Windows.Forms.Button();
+            this.Btn_ConvertInputToOutput = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Txt_InputPath
+            // 
+            this.Txt_InputPath.Location = new System.Drawing.Point(76, 14);
+            this.Txt_InputPath.Name = "Txt_InputPath";
+            this.Txt_InputPath.ReadOnly = true;
+            this.Txt_InputPath.Size = new System.Drawing.Size(507, 20);
+            this.Txt_InputPath.TabIndex = 0;
+            // 
+            // Txt_OutputPath
+            // 
+            this.Txt_OutputPath.Location = new System.Drawing.Point(76, 40);
+            this.Txt_OutputPath.Name = "Txt_OutputPath";
+            this.Txt_OutputPath.ReadOnly = true;
+            this.Txt_OutputPath.Size = new System.Drawing.Size(346, 20);
+            this.Txt_OutputPath.TabIndex = 1;
+            // 
+            // Txt_OutputFileName
+            // 
+            this.Txt_OutputFileName.Location = new System.Drawing.Point(428, 40);
+            this.Txt_OutputFileName.Name = "Txt_OutputFileName";
+            this.Txt_OutputFileName.Size = new System.Drawing.Size(155, 20);
+            this.Txt_OutputFileName.TabIndex = 2;
+            this.Txt_OutputFileName.TextChanged += new System.EventHandler(this.Txt_OutputFileName_TextChanged);
+            // 
+            // Btn_BrowseOutput
+            // 
+            this.Btn_BrowseOutput.Location = new System.Drawing.Point(589, 38);
+            this.Btn_BrowseOutput.Name = "Btn_BrowseOutput";
+            this.Btn_BrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.Btn_BrowseOutput.TabIndex = 3;
+            this.Btn_BrowseOutput.Text = "Browse";
+            this.Btn_BrowseOutput.UseVisualStyleBackColor = true;
+            this.Btn_BrowseOutput.Click += new System.EventHandler(this.Btn_BrowseOutput_Click);
+            // 
+            // Btn_BrowseInput
+            // 
+            this.Btn_BrowseInput.Location = new System.Drawing.Point(589, 12);
+            this.Btn_BrowseInput.Name = "Btn_BrowseInput";
+            this.Btn_BrowseInput.Size = new System.Drawing.Size(75, 23);
+            this.Btn_BrowseInput.TabIndex = 4;
+            this.Btn_BrowseInput.Text = "Browse";
+            this.Btn_BrowseInput.UseVisualStyleBackColor = true;
+            this.Btn_BrowseInput.Click += new System.EventHandler(this.Btn_BrowseInput_Click);
+            // 
+            // Btn_ConvertInputToOutput
+            // 
+            this.Btn_ConvertInputToOutput.Enabled = false;
+            this.Btn_ConvertInputToOutput.Location = new System.Drawing.Point(76, 67);
+            this.Btn_ConvertInputToOutput.Name = "Btn_ConvertInputToOutput";
+            this.Btn_ConvertInputToOutput.Size = new System.Drawing.Size(588, 23);
+            this.Btn_ConvertInputToOutput.TabIndex = 5;
+            this.Btn_ConvertInputToOutput.Text = "Convert";
+            this.Btn_ConvertInputToOutput.UseVisualStyleBackColor = true;
+            this.Btn_ConvertInputToOutput.Click += new System.EventHandler(this.Btn_ConvertInputToOutput_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 99);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_ConvertInputToOutput);
+            this.Controls.Add(this.Btn_BrowseInput);
+            this.Controls.Add(this.Btn_BrowseOutput);
+            this.Controls.Add(this.Txt_OutputFileName);
+            this.Controls.Add(this.Txt_OutputPath);
+            this.Controls.Add(this.Txt_InputPath);
+            this.Name = "Form1";
+            this.Text = "Alcolizer File-based Unit Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_InputPath;
+        private System.Windows.Forms.TextBox Txt_OutputPath;
+        private System.Windows.Forms.TextBox Txt_OutputFileName;
+        private System.Windows.Forms.Button Btn_BrowseOutput;
+        private System.Windows.Forms.Button Btn_BrowseInput;
+        private System.Windows.Forms.Button Btn_ConvertInputToOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlcolizerFileBasedUnitConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Tuple<string, float>> unitConversionsTuple =
+            new List<Tuple<string, float>>
+            {
+                new Tuple<string, float>("ug/100mL", 100000f),
+                new Tuple<string, float>("mg/L", 1000f),
+                new Tuple<string, float>("g/210L", 210f),
+                new Tuple<string, float>("g/230L", 230f),
+                new Tuple<string, float>("g/dL", 0.1f),
+                new Tuple<string, float>("ug/L", 1000000f),
+                new Tuple<string, float>("g/L", 1f)
+            };
+
+        private bool VerifyNotEmptyTextBoxes()
+        {
+            //Eg. Z:\
+            if(Txt_InputPath.Text.Length >= 3 
+                && Txt_OutputPath.Text.Length >= 3
+                && Txt_OutputFileName.Text.Length >= 1)
+            {
+                Btn_ConvertInputToOutput.Enabled = true;
+                return true;
+            }
+            else
+            {
+                Btn_ConvertInputToOutput.Enabled = false;
+                return false;
+            }
+        }
+
+        private void Btn_BrowseInput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+
+            if(dialogResult == DialogResult.OK 
+                && openFileDialog.CheckFileExists 
+                && openFileDialog.CheckPathExists)
+            {
+                Txt_InputPath.Text = openFileDialog.FileName;
+            }
+
+            VerifyNotEmptyTextBoxes();
+        }
+
+        private void Btn_BrowseOutput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+
+            if(dialogResult == DialogResult.OK)
+            {
+                Txt_OutputPath.Text = folderBrowserDialog.SelectedPath;
+            }
+
+            VerifyNotEmptyTextBoxes();
+        }
+
+        private void Btn_ConvertInputToOutput_Click(object sender, EventArgs e)
+        {
+            string outputFilePath = Txt_OutputPath.Text + @"\" + Txt_OutputFileName.Text + ".txt";
+            Console.WriteLine(outputFilePath);
+
+            if (VerifyNotEmptyTextBoxes())
+            {
+                if (File.Exists(outputFilePath))
+                {
+                    DialogResult dialogResult = MessageBox.Show(outputFilePath + " already exists. Would you like to overwrite this file?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        Console.WriteLine("Overwriting...");
+
+                        ConvertFileProcess();
+                    }
+                }
+                else
+                {
+                    ConvertFileProcess();
+                }
+            }
+        }
+
+        private void ConvertFileProcess()
+        {
+            try
+            {
+                string[] inputLines = File.ReadAllLines(Txt_InputPath.Text);
+                List<string> outputLines = new List<string>();
+
+                //Convert values process
+                foreach(string line in inputLines)
+                {
+                    string[] splitLine = line.Split(' ');
+
+                    for(int i = 0; i >= splitLine.Length-1; i++)
+                    {
+                        splitLine[i] = splitLine[i].Trim();
+                    }
+
+
+                    float amount = 0f;
+
+                    if(splitLine.Length != 3)
+                    {
+                        Console.WriteLine("Length error");
+                        outputLines.Add("Error");
+                        continue;
+                    }
+
+                    //Amount validation
+                    if(!float.TryParse(splitLine[0], out amount))
+                    {
+                        Console.WriteLine("Amount validation error");
+                        outputLines.Add("Error");
+                        continue;
+                    }
+
+                    //Unit validation
+                    if(!IsValidUnit(splitLine[1]) || !IsValidUnit(splitLine[2]))
+                    {
+                        Console.WriteLine("Unit validation error");
+                        outputLines.Add("Error");
+                        continue;
+                    }
+
+                    //if neither units are normalized, convert initial unit to normalized unit g/L
+                    if(splitLine[1] != "g/L" && splitLine[2] != "g/L")
+                    {
+                        amount = ReturnScaledAmount(splitLine[1], amount);
+                        amount = ConvertAmounts(splitLine[2], amount);
+                    }
+                    else
+                    {
+                        Console.WriteLine(amount);
+                        amount = ReturnScaledAmount(splitLine[1], amount);
+                        Console.WriteLine(amount);
+                        amount = ConvertAmounts(splitLine[2], amount);
+                        Console.WriteLine(amount);
+                        Console.WriteLine();
+                    }
+
+                    outputLines.Add(amount.ToString("0.000000")+" "+ splitLine[2]);
+                }
+
+                //Write list to file.
+                File.WriteAllLines(Txt_OutputPath.Text + @"\"+Txt_OutputFileName.Text+".txt", outputLines);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred during conversion." +
+                                "\n\n" + e.Message);
+            }
+        }
+
+        private float ConvertAmounts(string outputUnit, float inputAmount)
+        {
+            float amount = 0f;
+
+            foreach (Tuple<string, float> unitValue in unitConversionsTuple)
+            {
+                if (unitValue.Item1 == outputUnit)
+                {
+                    amount = unitValue.Item2 / inputAmount;
+                }
+            };
+
+            return amount;
+        }
+
+        private float ReturnScaledAmount(string inputUnit, float inputAmount)
+        {
+            float amount = 0f;
+
+            foreach (Tuple<string, float> unitValue in unitConversionsTuple)
+            {
+                if(string.Equals(unitValue.Item1, inputUnit))
+                {
+                    amount = unitValue.Item2 / inputAmount;
+                    break;
+                }
+            };
+
+            return amount;
+        }
+
+        private bool IsValidUnit(string inputUnit)
+        {
+            bool flag = false;
+
+            foreach (Tuple<string, float> unitValue in unitConversionsTuple)
+            {
+
+                if(string.Equals(unitValue.Item1, inputUnit))
+                {
+                    flag = true;
+                    continue;
+                };
+            };
+
+            return flag;
+        }
+
+        private void Txt_OutputFileName_TextChanged(object sender, EventArgs e)
+        {
+            VerifyNotEmptyTextBoxes();
+        }
+    }
+}
